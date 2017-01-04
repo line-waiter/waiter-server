079daff5978897dfe62e2028f3c93444f14ba8c4
Dillon Corkran <dcorkran972@gmail.com>
2017-01-03 18:00:15 -0700

    :bug: fixed cors bug

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.user(cors());
+app.use(cors());
 
 app.use('/', index);
 app.use('/users', users);
