4c45c88482576d89873126ecad816852c0a54a8d
Dillon Corkran <dcorkran972@gmail.com>
2017-01-03 19:12:43 -0700

    :sparkles: prepped post user route for deployement

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var queryFunctions = require('../db/queries');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,7 +8,12 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req.body);
-  res.render('index', { title: 'Express' });
+  if(queryFunctions.comparePassword(req.body)){
+    queryFunctions.postNewUser(req.body);
+    res.json('done on server!');
+  }else{
+    console.log("passwords don't match");
+    res.json(`passwords don't match`);
+  }
 });
 module.exports = router;
