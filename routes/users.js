1c1089dfe862cd833acd7e411d53639a84d64794
ikobdk <ikobidike@gmail.com>
2017-01-05 11:59:18 -0700

    routes for selecting all or one job(s) added

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var queryFunctions = require('../db/queries');
+var knex = require('../db/knex')
 
-/* GET users listing. */
-router.get('/jobs', function(req, res, next) {
-  //res.send('respond with a resource');
-  res.render('job')
-});
-
-router.post('/jobs/newjob',function(req,res){
-  console.log(req.body);
-  queryFunctions.postNewJob(req.body)
-  .then((all)=>{
-    res.json(all)
-  });
-});
+/*This is the sign up route*/
 
 router.post('/', function(req, res, next) {
   if(queryFunctions.comparePassword(req.body)){
@@ -27,4 +16,44 @@ router.post('/', function(req, res, next) {
     res.json(`passwords don't match`);
   }
 });
+
+/*This route posts new jobs to the data base*/
+
+router.post('/jobs',function(req,res){
+  queryFunctions.postNewJob(req.body)
+  .then((all)=>{
+    res.json(all)
+  });
+});
+
+
+/*This route gets all the jobs from the location and the jobs tables*/
+
+router.get('/jobs',function(req,res){
+    queryFunctions.getAllJobs()
+    .then((jobs)=>{
+    res.json(jobs)
+  })
+})
+
+/*This route gets one job*/
+
+router.get('/jobs/:id',function(req,res){
+  queryFunctions.getOneJob(req.params.id)
+  .then((job)=>{
+    res.json(job)
+  })
+})
+
+/*some nifty stuff :-P*/
+// router.get('/jobs/newjob/:id',function(req,res){
+//   knex('location').select().where('id',req.params.id).then((result)=>{
+//     knex('job').select().where('location_id',result[0].id).then((result2)=>{
+//       //res.json(result2)
+//       res.json([result,result2])
+//     })
+//   })
+// })
+
+
 module.exports = router;
