d3618607b8801da324716729988d349625d06741
Dillon Corkran <dcorkran972@gmail.com>
2017-01-04 19:23:25 -0700

    :bug: fixed dotenv error

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+require("dotenv").config();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
