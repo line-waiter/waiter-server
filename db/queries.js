0b70b41e7d6702cfc1ebf3d2c63b33b38f2d5f04
Murph Grainger <murph.grainger@gmail.com>
2017-01-08 10:13:06 -0600

    changed back to dillon's commit

--- db/queries.js
+++ db/queries.js
@@ -3,7 +3,6 @@ const bcrypt = require('bcryptjs');
 
 module.exports = {
     postNewJob: function(body) {
-        console.log(body);
         return knex('location')
             .insert({
                 name: body.name,
