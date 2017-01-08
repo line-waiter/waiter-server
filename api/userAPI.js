5d15de26f6813456194dbf9f9f70150edd538d88
Murph Grainger <murph.grainger@gmail.com>
2017-01-08 10:28:48 -0600

    added console log to debug

--- api/userAPI.js
+++ api/userAPI.js
@@ -2,27 +2,27 @@ var express = require('express');
 var router = express.Router();
 var queries = require('../db/queries');
 
-router.get('/request',function(req,res,next){
-  queries.getRequestsByUser(req.signedCookies.userID)
-  .then((data)=>{
-    console.log(data);
-    res.json(data);
-  });
+router.get('/request', function(req, res, next) {
+    queries.getRequestsByUser(req.signedCookies.userID)
+        .then((data) => {
+            console.log(data);
+            res.json(data);
+        });
 });
 
-router.get('/job',function(req,res,next){
-  queries.getJobsByUser(req.signedCookies.userID)
-  .then((data)=>{
-    console.log(data);
-    res.json(data);
-  });
+router.get('/job', function(req, res, next) {
+    queries.getJobsByUser(req.signedCookies.userID)
+        .then((data) => {
+            console.log(data);
+            res.json(data);
+        });
 });
 
-router.delete('/job',function(req,res,next){
-  queries.deleteJob(req.body.id)
-  .then(()=>{
-    res.json('Job Deleted');
-  });
+router.delete('/job', function(req, res, next) {
+    queries.deleteJob(req.body.id)
+        .then(() => {
+            res.json('Job Deleted');
+        });
 });
 
 
