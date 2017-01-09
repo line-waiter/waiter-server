63f28595e406a787a6aa2b0a6ee48042f9d988ca
Dillon Corkran <dcorkran972@gmail.com>
2017-01-08 21:13:26 -0700

    :sparkles: added user fname route

--- api/userAPI.js
+++ api/userAPI.js
@@ -25,6 +25,13 @@ router.delete('/job', function(req, res, next) {
         });
 });
 
+router.get('/', function(req, res, next) {
+    queries.getOneUser(req.signedCookies.userID[0])
+        .then((data) => {
+            res.json(data);
+        });
+});
+
 
 
 module.exports = router;
